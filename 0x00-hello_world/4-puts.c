8a7786dd3f1e91a28a80c7f1f88feb8c441d6f01
fonzk <alfdiazr@hotmail.com>
2020-06-10 16:00:14 -0500

    fitcommet

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - use put no allow use printf
+ * Description: uses puts to print message
+ * Return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
