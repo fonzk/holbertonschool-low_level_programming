84cc81ea6be11c10719e50af77b1d2e0d2437fb2
fonzk <alfdiazr@hotmail.com>
2020-06-10 18:24:56 -0500

    sss

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,10 @@
  * description: using write to stdout message error
  * Return: 1 one
 */
+
 int main(void)
 {
-int lol = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", lol);
+int lol = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", lol);
 return (1);
 }
