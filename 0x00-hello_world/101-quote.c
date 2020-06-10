80050fdbf239e21e0571fe4081ea320af50bfd6d
fonzk <alfdiazr@hotmail.com>
2020-06-10 18:17:25 -0500

    xxxx

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,5 +11,4 @@ int main(void)
 int lol = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
 write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", lol);
 return (1);
-
 }
