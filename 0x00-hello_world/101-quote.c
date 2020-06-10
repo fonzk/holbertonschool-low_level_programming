b0d2e9d55e6bf195e669185ef5ce7bc12f0eb7b5
fonzk <alfdiazr@hotmail.com>
2020-06-10 18:09:31 -0500

    aaaaa

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 /**
  * main - writes text to stdout
  * description: using write to stdout message error
- * Return: 1
+ * Return: return 1
 */
 int main(void)
 {
 
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 0);
-return (1);
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+return 1;
 
 }
