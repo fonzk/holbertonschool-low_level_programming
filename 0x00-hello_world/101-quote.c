bea2e7fa97449d77047d13f857956a5463250330
fonzk <alfdiazr@hotmail.com>
2020-06-10 17:30:11 -0500

    fiz

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-
 /**
  * main - writes text to stdout
  * description: using write to stdout message error
@@ -7,8 +6,6 @@
 */
 int main(void)
 {
-
 write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 return (1);
-
 }
