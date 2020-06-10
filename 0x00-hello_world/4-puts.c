4811c1a071054d74aaa750a3ab9dbd53b25455f2
fonzk <alfdiazr@hotmail.com>
2020-06-10 15:58:40 -0500

    hot puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - puts line to stdout
- * Description: uses puts to output fixed message
- * Return: void
- */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
