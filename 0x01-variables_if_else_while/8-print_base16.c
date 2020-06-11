b5686b7114c56c1d983ac1892be3657973806099
fonzk <alfdiazr@hotmail.com>
2020-06-11 17:21:52 -0500

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - use for
+ * Description: use putchar to print the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9' ; a++)
+putchar(a);
+for (a = 'a'; a < 'g' ; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
