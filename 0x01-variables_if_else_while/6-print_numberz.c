014c2cb4c70d9d53c7d5ea965ee1d22d56474c61
fonzk <alfdiazr@hotmail.com>
2020-06-11 16:49:45 -0500

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - use for
+ * Description: use putchar to print the alphabet except q and e
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = '0' ; a <= '9' ; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
