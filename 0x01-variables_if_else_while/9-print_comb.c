8c47f02217cc1a353a3426f749e7e82ecb6d145b
fonzk <alfdiazr@hotmail.com>
2020-06-11 17:30:08 -0500

    fixall

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@
 int main(void)
 {
 int a;
-for (a = '0'; a <= '9' ; a++)
+for (a = '0'; a <= '8' ; a++)
 {
 putchar(a);
 putchar(',');
 putchar(' ');
 }
-putchar('\n');
+putchar('9');
 return (0);
 }
