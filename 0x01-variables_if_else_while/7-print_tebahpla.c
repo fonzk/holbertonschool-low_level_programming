3c0d4a6f630886417255144addcd262395d414fa
fonzk <alfdiazr@hotmail.com>
2020-06-11 16:53:57 -0500

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - use for
+ * Description: use putchar to print the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 'z'; a >= 'a' ; a--)
+putchar(a);
+putchar('\n');
+return (0);
+}
