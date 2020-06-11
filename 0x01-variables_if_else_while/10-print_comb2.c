149d026b2cd17489df9a3e0adaba9581b3a10609
fonzk <alfdiazr@hotmail.com>
2020-06-11 18:07:52 -0500

    10. 00...99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - use for
+ * Description: use putchar to numbers
+ * Return: 0
+ */
+int main(void)
+{
+int a, b;
+for (a = '0'; a <= '9' ; a++)
+for (b = '0'; b <= '9' ; b++)
+{
+putchar(a);
+putchar(b);
+if (a != '9' || b != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
