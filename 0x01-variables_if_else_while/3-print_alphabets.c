759209d58b7a01ab294045fc8d35560b7ad97091
fonzk <alfdiazr@hotmail.com>
2020-06-11 16:15:09 -0500

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - use for
+ * Description: use putchar to print the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 'a'; a <= 'z' ; a++)
+putchar(a);
+for (a = 'A'; a <= 'Z' ; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
