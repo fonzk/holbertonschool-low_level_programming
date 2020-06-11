58fe89ba6723688b6ea857cad71f292b5fb07ba4
fonzk <alfdiazr@hotmail.com>
2020-06-11 15:52:06 -0500

    2. I sometimes suffer from insomnia.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - use for
+ * Description: use putchar to print the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = "a"; a <= "z" ; a++)
+putchar(a);
+putchar("\n");
+return (0);
+}
