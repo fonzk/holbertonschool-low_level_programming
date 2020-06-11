1b4fe7edf2b568e0e027a12e290b1ecccc69aea7
fonzk <alfdiazr@hotmail.com>
2020-06-11 17:34:54 -0500

    9. Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,15 @@
 int main(void)
 {
 int a;
-for (a = '0'; a <= '8' ; a++)
+for (a = '0'; a <= '9' ; a++)
 {
 putchar(a);
+if (a != '9')
+{
 putchar(',');
 putchar(' ');
 }
-putchar('9');
+}
+putchar('\n');
 return (0);
 }
