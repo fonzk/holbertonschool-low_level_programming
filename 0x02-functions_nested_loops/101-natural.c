e804bed10b56c8eaf0ea594efc3dffc91dd33718
fonzk <alfdiazr@hotmail.com>
2020-06-16 16:53:56 -0500

    sssss

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,8 @@
 
 /**
  * main -  the sum of all the multiples of 3 or 5 below 1024
- * Return: void
+ *
+	* Return: void
  */
 int main(void)
 {
@@ -12,10 +13,11 @@ int main(void)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
+		printf("%i\n", i);
 			suma += i;
 		}
 	}
-		printf("%i\n", suma);
+		printf("total%i\n", suma);
 
 			return (0);
 }
