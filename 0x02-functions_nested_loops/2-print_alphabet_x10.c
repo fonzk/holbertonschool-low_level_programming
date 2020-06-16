fd2f8c128b7022b33aabb0ffcf50c12eda968a98
fonzk <alfdiazr@hotmail.com>
2020-06-16 06:40:32 -0500

    ssss

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
+
 /**
  * print_alphabet_x10 - use _putchar to print alphabetx10
  * Description: use _putchar to print a to z x 10
  * Return: print every alphabet
  */
+
 void print_alphabet_x10(void)
 {
 for (int i = 0; i <= 9 ; i++)
