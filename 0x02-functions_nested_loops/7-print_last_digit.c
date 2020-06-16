f9e6ae1628a9c9bbfcb57a218f8187267453ff6d
fonzk <alfdiazr@hotmail.com>
2020-06-16 07:46:48 -0500

    locuras

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,13 @@
  */
 int print_last_digit(int n)
 {
-	int l, p;
-
-	l = n % 10;
-	if (l <  0)
-		l = -l;
-	p = '0' + l;
-	_putchar(p);
-	return (l);
+int Number, Ldigit, p;
+if (n < 0)
+Number = -1 * n;
+else
+Number = n;
+Ldigit	= Number % 10;
+p = '0' + Ldigit;
+_putchar(p);
+return (Ldigit);
 }
