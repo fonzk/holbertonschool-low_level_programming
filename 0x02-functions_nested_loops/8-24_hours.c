0ac9d3a01e546fc087390097baf92bef28340677
fonzk <alfdiazr@hotmail.com>
2020-06-15 22:26:21 -0500

    agent Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - print minute
+ *
+ * descripcion: 24h minute by minute
+ * Return: none
+ */
+void jack_bauer(void)
+{
+for (int h = 0 ; h < 24; h++)
+for (int m = 0; m < 60; m++)
+{
+_putchar(h / 10 + '0');
+_putchar(h % 10 + '0');
+_putchar(':');
+_putchar(m / 10 + '0');
+_putchar(m % 10 + '0');
+_putchar('\n');
+}
+}
