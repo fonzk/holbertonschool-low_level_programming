bf1d7775d2938e4e562e5444cfb7c753c7157ba2
fonzk <alfdiazr@hotmail.com>
2020-06-16 06:36:51 -0500

    fix alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,13 @@
 /**
  * print_alphabet_x10 - use _putchar to print alphabetx10
  * Description: use _putchar to print a to z x 10
- * Return: none
+ * Return: print every alphabet
  */
 void print_alphabet_x10(void)
 {
 for (int i = 0; i <= 9 ; i++)
 {
-for (int letter = 'a'; letter <= 'z' ; letter++)
+for (int letter = 97 ; letter <= 122 ; letter++)
 _putchar(letter);
 _putchar('\n');
 }
