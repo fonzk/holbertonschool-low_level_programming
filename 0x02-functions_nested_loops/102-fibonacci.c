1443fa963fb1a58e36b6ba3d966c733992644e97
fonzk <alfdiazr@hotmail.com>
2020-06-16 18:08:25 -0500

    fibonachi

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -  the first 50 fibonnaci numbers
+ *
+	* Return: void
+ */
+int main(void)
+{
+	long u1 = 2;
+	int c = 3;
+	long u2 = 3;
+	long s = 3;
+
+printf("1, 2, ");
+	for (; c < 50; c++)
+	{
+		printf("%ld, ", s);
+		s = u1 + u2;
+		u1 = u2;
+		u2 = s;
+	}
+		printf("%ld\n", s);
+	return (0);
+}
