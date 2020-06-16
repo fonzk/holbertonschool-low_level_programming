495de81276e1e36ee94c23c15205d8d8c8a215d9
fonzk <alfdiazr@hotmail.com>
2020-06-16 09:17:17 -0500

    ssssss

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,20 @@
  */
 void print_to_98(int n)
 {
-int num = n;
-if (num <= 0 || num <= 98)
+int m = n;
+if (m <= 0 || m <= 98)
 {
-for (; num < 98; num++)
+for (; m < 98; m++)
 {
-printf("%i, ", num);
+printf("%i, ", m);
 }
 printf("%i\n", 98);
 }
 else
 {
-for (; num > 98; num--)
+for (; m > 98; m--)
 {
-printf("%i, ", num);
+printf("%i, ", m);
 }
 }
 }
