d4df3dfff87ab7f42665fb0950ee4f004868289e
fonzk <alfdiazr@hotmail.com>
2020-06-16 19:37:25 -0500

    103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
+
 /**
- * main -  the first 98 fibonnaci numbers
+ *main - prints even fibonacci numbers that are under 4,000,000
  *
-	* Return: void
+ *
+ *Return: returns 0
  */
+
 int main(void)
 {
-int i = 0;
-unsigned long n1, n2;
-n1 = 1;
-n2 = 2;
-while (i < 100)
-{
-	printf("%lu\n %lu\n", n1, n2);
-	n1 += n2;
-	n2 += n1;
-	i += 2;
-}
+	long f, l, i, sum;
+
+	f = 3;
+	i = 1;
+	l = 2;
+	sum = 2;
+
+	while (f < 3000000)
+	{
+		if (f % 2 == 0)
+			sum += f;
+		i = l;
+		l = f;
+		f = i + l;
+	}
+	if (f % 2 == 0)
+		sum += f;
+		printf("%ld\n", sum);
+
 	return (0);
 }
