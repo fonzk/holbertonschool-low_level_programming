8d457899ffd8d798c1cdf69e256f0a8fb2b67a0d
fonzk <alfdiazr@hotmail.com>
2020-06-18 14:01:43 -0500

    fizzbuzzer

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,30 +8,31 @@
  */
 int main(void)
 {
-int i = 1;
+	int i = 1;
 
-for (; i <= 100; i++)
-{
-if (i % 3 != 0 && i % 5 != 0)
-{
-printf("%i", i);
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz");
-}
-else if (i % 3 == 0)
-{
-printf("Fizz");
-}
-else if (i % 5 == 0)
-{
-printf("Buzz");
-}
-if (i != 100)
-{
-printf(" ");
-}
-printf("\n");
-return (0);
+	for (; i <= 100; i++)
+	{
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%i", i);
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
 }
