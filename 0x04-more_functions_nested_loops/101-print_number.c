afffb43f8674aaa5cf829a5b8a105e3af495e6e4
fonzk <alfdiazr@hotmail.com>
2020-06-18 18:55:10 -0500

    final

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,61 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * mypow - Quick pow implementation.
+ * Description: Multiples number by itself a number of times
+ * designated by the exponent.
+ *
+ * @base: base number
+ * @exponent: exponent, number of times to multiply base by itself
+ * Return: returns zero
+ */
+long mypow(long base, long exponent)
+{
+	long val = 1;
+
+	while (exponent > 0)
+	{
+		val *= base;
+		exponent--;
+	}
+	return (val);
+
+}
+
+/**
+ * print_number - takes in number, outputs  using putchar
+ *
+ * @n: number to putchar
+ * Return: void
+ */
+void print_number(int n)
+{
+	long digits, stop, i, num, temp;
+
+	i = 1;
+	digits = 0;
+	stop = 0;
+	temp = n;
+	if (temp < 0)
+	{
+		_putchar('-');
+		temp = (temp - (temp * 2));
+	}
+	while (stop == 0)
+	{
+		if ((temp / mypow(10, i)) < 1)
+			break;
+		i++; //2 3
+		digits++; //1 2 
+	}
+
+	num = temp / mypow(10, digits);
+	_putchar(num + '0');
+
+	while (digits > 0)
+	{
+		num = temp % mypow(10, digits--);
+		num = num / mypow(10, digits);
+		_putchar(num + '0');
+	}
+}
