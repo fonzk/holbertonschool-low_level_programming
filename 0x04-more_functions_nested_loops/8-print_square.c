7be604d78a736f965dde524adc2eb65f4d196edc
fonzk <alfdiazr@hotmail.com>
2020-06-18 13:10:40 -0500

    8. You are

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_square -  prints a square, followed by a new line.
+ * @size: this determenate the area of square
+	* Return: none.
+ */
+void print_square(int size)
+{
+	int h, w;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (h = 0; h < size; h++)
+		{
+			for (w = 0; w < size; w++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
