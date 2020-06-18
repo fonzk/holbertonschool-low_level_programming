68cc503b1ccfdf6a91d77d9758ed35b48f7b5581
fonzk <alfdiazr@hotmail.com>
2020-06-18 13:51:28 -0500

    canbefizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints the numbers from 1 to 100,
+ * Description:  A column of asterisks on the left side,
+ * Return: Always 0.
+ */
+int main(void)
+{
+for (int i = 1; i <= 100; i++)
+{
+if (i % 3 != 0 && i % 5 != 0)
+{
+printf("%i", i);
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+if (i != 100)
+{
+printf(" ");
+}
+printf("\n");
+return (0);
+}
