b93dc823af14f8a8c3961a0b55fbc275c4ef084c
fonzk <alfdiazr@hotmail.com>
2020-06-22 08:29:45 -0500

    task1_0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98.
+ * @n: a variable as referencia
+ * followed by a new line.
+	* Return: none.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
