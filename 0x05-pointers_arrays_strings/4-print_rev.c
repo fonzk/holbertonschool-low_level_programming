97c2d415b849f3fa6634111c6b1d2edf39ceebce
fonzk <alfdiazr@hotmail.com>
2020-06-23 07:26:51 -0500

    Task4_3

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,12 +14,12 @@ int i = 0; /* counter*/
 
 char nully = '\0'; /* this is the last char as null*/
 
-while (s[i] != nully)
+while (*(s + i) != nully)
 	i++;
 int z = i - 1;
 
 for (; z >= 0; z--)
-	_putchar(s[z]);
-_putchar(10);
+	_putchar(*(s + z));
+_putchar('\n');
 
 }
