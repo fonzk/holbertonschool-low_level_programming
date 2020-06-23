81c8463fd1054c446b9135ccc7b71950a04c81a9
fonzk <alfdiazr@hotmail.com>
2020-06-23 08:36:33 -0500

    Task7_1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,15 +10,15 @@
 
 void puts_half(char *str)
 {
-int len = 0, i = 0, s = 0;
+	int len = 0, i = 0, s = 0;
 
-while (str[len] != '\0')
-len++;
-s = len  / 2; /*start to print*/
+	while (str[len] != '\0')
+	len++;
+s = len  / 2;/*start to print*/
 if (len % 2 != 0)
-s = (len - 1) / 2; /*odd number start*/
+	s = (len - 1) / 2;/*odd number start*/
 i = s;
 for (; i < len; i++)
-_putchar(str[i]);
-_putchar(10);
+	_putchar(str[i]);
+_putchar('\n');
 }
