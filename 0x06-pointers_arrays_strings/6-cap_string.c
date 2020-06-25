61d26e733368733ba94f37dbfadf27fa2763e79e
fonzk <alfdiazr@hotmail.com>
2020-06-25 18:43:47 -0500

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+	* cap_string - a-zA-Z change
+	* @a: char to upper
+	* Return: int
+	*/
+char *cap_string(char *a)
+{
+	char sep[] = {32, 9, 10, ',', ';', '.', '!', '?', 34, 40, 41, 123, 125};
+	int i = 0, j;
+
+	for (; *(a + i); i++)
+	{
+		for (j = 0; *(sep + j); j++)
+		{
+			if (a[i + 1] >= 'a' && a[i + 1] <= 'z' && a[i] == sep[j])
+			{
+				a[i] -= 32;
+			}
+		}
+		return (a);
+	}
