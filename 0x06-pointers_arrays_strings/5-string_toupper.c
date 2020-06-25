aef1b8e367f4cb2c808f28251f6509635199f975
fonzk <alfdiazr@hotmail.com>
2020-06-25 17:49:18 -0500

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+	* string_toupper - a-zA-Z
+	* @a: char
+	*
+	*
+	* Return: int
+	*/
+char *string_toupper(char *a)
+{
+	for (int i = 0; *(a + i); i++)
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+		a[i] -= 32;
+		}
+
+	}
+
+return (a);
+	}
+
