aa5db2e02d35d03aca65701b6c5f3c0caaaffce8
fonzk <alfdiazr@hotmail.com>
2020-06-25 14:57:20 -0500

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+	* reverse_array - compare.
+	* @a: string to reverse
+	* @n: len de string
+	*
+	* Return: int
+	*/
+void reverse_array(int *a, int n)
+{
+int i = 0, j = 0;
+int b[n];
+for (; i < n; i++)
+{
+	b[i] = a[i];
+}
+for (; j < n; i++)
+{
+	 a[i] = b[n-i];
+}
+}
