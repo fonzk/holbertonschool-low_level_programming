9e62d80712d3826af10bca1b3dbd938275c2afe2
fonzk <alfdiazr@hotmail.com>
2020-06-25 19:15:57 -0500

    5556

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
 	*/
 char *cap_string(char *a)
 {
-	char sep[] = {32, 9, 10, ',', ';', '.', '!', '?', 34, 40, 41, 123, 125};
+	char sep[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
 	int i, j, p;
 
 	for (i = 0 ; *(a + i); i++)
@@ -15,6 +15,7 @@ char *cap_string(char *a)
 		for (j = 0; *(sep + j); j++)
 		{
 			p = i + 1;
+
 			if (a[p] != '\0' && a[p] >= 'a' && a[p] <= 'z' && a[i] == sep[j])
 			{
 				a[p] -= 32;
