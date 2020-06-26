3d88174d75eb1e308f31e88e132b696d21e754db
fonzk <alfdiazr@hotmail.com>
2020-06-25 20:18:46 -0500

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+
+#include "holberton.h"
+/**
+	*leet - change letters by numbers
+	*@s1: pointer parameter"
+	*Description: change letters by numbers
+	*Return: return pointer
+	*/
+char *leet(char *s1)
+{
+	int i, j;
+		char code[10][2] = {
+				{'a', '4'},
+				{'A', '4'},
+				{'e', '3'},
+				{'E', '3'},
+				{'o', '0'},
+				{'O', '0'},
+				{'t', '7'},
+				{'T', '7'},
+				{'l', '1'},
+				{'L', '1'}
+				};
+for (i = 0; s1[i]; i++)
+{
+for (j = 0; code[j][0]; j++)
+{
+	if (s1[i] == code[j][0])
+	{
+		s1[i] = code[j][1];
+	}
+
+}
+
+}
+
+	return (s1);
+}
