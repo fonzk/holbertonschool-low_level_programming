5979d9350aa5da83b957bf88fe5fe8c9cd2ad51c
fonzk <alfdiazr@hotmail.com>
2020-06-25 19:56:29 -0500

    prueba

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,61 +1,27 @@
 #include "holberton.h"
+
 /**
- *_strlen - reset number
- *Description: This function return a length for some string
- *@s: pointer char
- *Return: int length
- */
-
-int _strlen(char *s)
+	* cap_string - a-zA-Z change
+	* @a: char to upper
+	* Return: int
+	*/
+char *cap_string(char *a)
 {
-	int len = 0;
+	char sep[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t', '!'};
+	int i, j, p;
 
-	while (*s++)
+	for (i = 0 ; *(a + i); i++)
 	{
-		len++;
-	}
-	return (len);
-}
-/**
- *cap_string - changes all lowercase letters
- *@s1: pointer parameter"
- *Description: changes all lowercase letters
- *Return: return pointer
- */
-char *cap_string(char *s1)
-{
-	int i, j;
-
-	for (i = 0; i < _strlen(s1) - 1; i++)
-	{
-		if (
-			s1[i] == ' ' ||
-			s1[i] == '\t' ||
-			s1[i] == '\n' ||
-			s1[i] == ',' ||
-			s1[i] == ';' ||
-			s1[i] == '.' ||
-			s1[i] == '!' ||
-			s1[i] == '?' ||
-			s1[i] == '"' ||
-			s1[i] == '(' ||
-			s1[i] == ')' ||
-			s1[i] == '{' ||
-			s1[i] == '}' ||
-			i == 0
-		)
+		for (j = 0; *(sep + j); j++)
 		{
-			for (j = 'a'; j <= 'z'; j++)
+			p = i + 1;
+
+			if (a[p] != '\0' && a[p] >= 'a' && a[p] <= 'z' && a[i] == sep[j] )
 			{
-				if (s1[i + 1] == j && i != 0)
-				{
-					s1[i + 1] = j - 32;
-				} else if (s1[i] == j && i == 0)
-				{
-					s1[i] = j - 32;
-				}
+				a[p] -= 32;
 			}
 		}
+
 	}
-	return (s1);
+	return (a);
 }
