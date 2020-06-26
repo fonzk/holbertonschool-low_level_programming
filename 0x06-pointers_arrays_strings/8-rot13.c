0015cda010112d670aebb56ec2041a92a2c4ed82
fonzk <alfdiazr@hotmail.com>
2020-06-25 20:43:12 -0500

    rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+
+#include "holberton.h"
+/**
+ *rot13 - Rotate letters
+ *@s1: pointer parameter
+ *Description: Rotate letters
+ *Return: return pointer
+ */
+char *rot13(char *s1)
+{
+	int i, j;
+		char code[2][53] = {
+			{"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"},
+			{"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"}
+																					};
+for (i = 0; s1[i]; i++)
+{
+for (j = 0; j < 52; j++)
+{
+	if (s1[i] == code[0][j])
+	{
+		s1[i] = code[1][j];
+	}
+
+}
+
+}
+
+	return (s1);
+}
